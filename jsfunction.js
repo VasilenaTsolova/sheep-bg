cbd21f20086f281823f175c6401f45822d81e7dd
VasilenaTsolova <49832741+VasilenaTsolova@users.noreply.github.com>
2019-12-10 23:41:11 +0200

    Add files via upload

---
+++ jsfunction.js
@@ -0,0 +1,9 @@
+document.getElementById("submit").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Your answer was sent successfully!");
+}
+   
+   function changecol(y) {
+  y.style.background = "pink";
+}
---
+++ toggle.js
@@ -0,0 +1,39 @@
+function toggch1 (image1)
+{
+    image1.src = "../photos/sheep12.jpg";
+}
+function toggback1 (image1)
+{
+    image1.src = "../photos/sheep1.jpg";
+
+}
+function toggch2 (image2)
+{
+    image2.src= "../photos/lamb2.jpg";
+}
+function toggback2 (image2)
+{
+    image2.src = "../photos/sheep22.jpg";
+
+}
+    function toggch3 (image3)
+{
+    image3.src= "../photos/sheep3.jpg";
+}
+function toggback3 (image3)
+{
+    image3.src = "../photos/lamb3.jpg";
+
+}
+function toggch4 (image4)
+{
+    image4.src= "../photos/hoof4.jpg";
+}
+function toggback4 (image4)
+{
+    image4.src = "../photos/wool4.jpg";
+
+}
+
+
+
